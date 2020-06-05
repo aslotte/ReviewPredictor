ddad1ab13ad1da6e670d4af779f98b113b02cd11
Alexander Slotte <alexander.slotte@gmail.com>
2020-06-05 17:32:41 -0400

    Added initial support for MLOps.NET integration

--- ReviewPredictor.Trainer/Program.cs
+++ ReviewPredictor.Trainer/Program.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.ML;
+using MLOps.NET;
+using MLOps.NET.Azure;
 using ReviewPredictor.Model;
 using System;
+using System.Threading.Tasks;
 
 namespace ReviewPredictor.Trainer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var connectionString = args[0];
+
+            var mlLifeCycleManager = new MLLifeCycleManager().UseAzureStorage(connectionString);
+
+            var experimentId = await mlLifeCycleManager.CreateExperimentAsync("Review Predictor");
+            var runId = await mlLifeCycleManager.CreateRunAsync(experimentId);
+
             var mlContext = new MLContext(seed: 1);
 
             //Load the data
@@ -32,6 +42,17 @@ namespace ReviewPredictor.Trainer
             //Evaluate
             var predicitions = model.Transform(trainTestSplit.TestSet);
             var metrics = mlContext.BinaryClassification.Evaluate(predicitions, labelColumnName: "Sentiment");
+
+            //Log metrics
+            await mlLifeCycleManager.LogMetricAsync(runId,
+                nameof(metrics.Accuracy),
+                metrics.Accuracy);
+
+            //Upload the model (artifact)
+
+            //Check - is current model better than production model?
+
+            //Save model as artifact for run
 
             Console.WriteLine("Training complete");
             Console.WriteLine($"Accuracy: {metrics.Accuracy}");
